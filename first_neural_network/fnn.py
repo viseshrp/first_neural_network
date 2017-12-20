b8c6fcec09740018fd5e3ca12c2fba50df0291ea
viseshrp <viseshrprasad@gmail.com>
2017-12-20 11:43:13 -0500

    Add forward propagation and find estimates

--- first_neural_network/fnn.py
+++ first_neural_network/fnn.py
@@ -1,7 +1,72 @@
 import numpy as np
 
-X = np.array(([3, 5], [5, 1], [10, 2]), dtype=float)
-print(X)
 
-y = np.array(([75], [82], [93]), dtype=float)
-print(y)
+class Neural_Network(object):
+
+    def __init__(self):
+        '''
+        declare hyperparameters to define structure
+        of network
+        all constant values
+        '''
+        self.input_layer_size = 2
+        self.output_layer_size = 1
+        self.hidden_layer_size = 3
+
+        # initialize weight matrices to random values
+        self.W1 = np.random.randn(self.input_layer_size, self.hidden_layer_size)
+        self.W2 = np.random.randn(self.hidden_layer_size, self.output_layer_size)
+
+    def forward(self, X):
+        '''pass multiple inputs at once
+        through Neural_Network using matrices.
+        Compute yHat or actual cost.'''
+        # find dot product of input matrix
+        # and weight matrix for first layer.
+        self.z2 = np.dot(X, self.W1)
+        # apply sigmoid to matrix
+        self.a2 = self.sigmoid(self.z2)
+        # multiply output by second layer of weights
+        self.z3 = np.dot(self.a2, self.W2)
+        # apply sigmoid again
+        yHat = self.sigmoid(self.z3)
+
+        return yHat
+
+    def sigmoid(z):
+        '''apply sigmoid activation
+        pass in a scalar/vector/matrix
+        using numpy, numpy applies sigmoid
+        to each element in the vector'''
+        return 1 / (1 + np.exp(-z))
+
+
+def log_it(text):
+    print(text)
+
+
+def main():
+    # Matrix of inputs
+    X = np.array(([3, 5], [5, 1], [10, 2]), dtype=float)
+    # print(X)
+
+    # matrix of (expected) outputs for the inputs above.
+    y = np.array(([75], [82], [93]), dtype=float)
+    # print(y)
+
+    '''Normalize values and bring them between 0 and 1, so as to
+    make them comparable'''
+    # divides each x1 value by highest x1 and each x2 value by highest x2
+    X = X / (np.amax(X, axis=0))
+    # do the same here
+    y = y / 100
+    # print(y / 100)
+
+    FNN = Neural_Network()
+
+    yHat = FNN.forward(X)
+
+    print(yHat)
+
+if __name__ == "__main__":
+    main()
